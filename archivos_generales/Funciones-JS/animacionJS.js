e4b3d486f0fd97b4f1a914fa21b3adfe987b9d99
Dioniso0 <30611028+Dioniso0@users.noreply.github.com>
2017-09-22 14:49:48 -0600

    Funcionamiento de Menu responsive - CSS - JS
    agregalo a la master

---
+++ archivos_generales/Funciones-JS/animacionJS.js
@@ -0,0 +1,25 @@
+(()=>{
+    var li  = document.querySelectorAll('#blqMen li');
+    var obj;
+    
+    document.getElementById('btnMenu').addEventListener('click',function(){
+        document.getElementById('sideBar').classList.add('act-menu');
+    })
+
+    
+
+    for(var i = 0; i < li.length; i++){
+        li[i].addEventListener('click',function(){  
+
+            obj = this.getAttribute('class').trim();
+
+            if(obj == 'activado' ){
+                this.classList.remove('activado');
+            }else{
+                
+                this.classList.add('activado');
+            }
+           
+        })
+    }
+})();
