ff76c57f71bd32d4a73d3c6d2a5b4ecc9160cfa1
Dioniso0 <30611028+Dioniso0@users.noreply.github.com>
2017-09-23 19:46:27 -0600

    vista de usuario a un 90%

--- archivos_generales/Funciones-JS/animacionJS.js
+++ archivos_generales/Funciones-JS/animacionJS.js
@@ -2,26 +2,22 @@
     var li  = document.querySelectorAll('#blqMen li');
     var obj;
 
-    document.getElementById('btnMenu').addEventListener('click',function(){
-        document.getElementById('sideBar').classList.toggle('act-menu');
-    })
+  
 
-    
+    // for(var i = 0; i < li.length; i++){
+    //     li[i].addEventListener('click',function(){  
 
-    for(var i = 0; i < li.length; i++){
-        li[i].addEventListener('click',function(){  
+    //         obj = this.getAttribute('class').trim();
 
-            obj = this.getAttribute('class').trim();
-
-            if(obj == 'activado' ){
-                this.classList.remove('activado');
-            }else{
+    //         if(obj == 'activado' ){
+    //             this.classList.remove('activado');
+    //         }else{
                 
-                this.classList.add('activado');
-            }
+    //             this.classList.add('activado');
+    //         }
            
-        })
-    }
+    //     })
+    // }
 
 
     
