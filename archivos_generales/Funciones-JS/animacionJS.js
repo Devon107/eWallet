45d716de6e7c92e3c6b6145e558643970b509424
Dioniso0 <rodrigolemus3098@gmail.com>
2017-10-01 18:23:02 -0600

    vistas listas

--- archivos_generales/Funciones-JS/animacionJS.js
+++ archivos_generales/Funciones-JS/animacionJS.js
@@ -20,60 +20,12 @@
     //     })
     // }
 
-    var uls = document.querySelectorAll('#VistaUl .Primer .sub');
-    var lis = document.querySelectorAll('#VistaUl .Primer');
+   
     // console.log(lis)
     // for(var i = 0; i<lis.length; i++){
         
     // }
-    lis[0].addEventListener('click',function(){
-        
-         let cl = uls[0].getAttribute('class').trim();
-       
-         if(cl == 'sub'){
-            uls[0].classList.remove('sub');
-            uls[0].classList.add('act');
-         }else if(cl == 'act'){
-            uls[0].classList.remove('act');
-            uls[0].classList.add('sub');
-         }
-     })
-     lis[1].addEventListener('click',function(){
-        
-         let cl = uls[1].getAttribute('class').trim();
-       
-         if(cl == 'sub'){
-            uls[1].classList.remove('sub');
-            uls[1].classList.add('act');
-         }else if(cl == 'act'){
-            uls[1].classList.remove('act');
-            uls[1].classList.add('sub');
-         }
-     })
-     lis[2].addEventListener('click',function(){
-        
-         let cl = uls[2].getAttribute('class').trim();
-       
-         if(cl == 'sub'){
-            uls[2].classList.remove('sub');
-            uls[2].classList.add('act');
-         }else if(cl == 'act'){
-            uls[2].classList.remove('act');
-            uls[2].classList.add('sub');
-         }
-     })
-     lis[3].addEventListener('click',function(){
-        
-         let cl = uls[3].getAttribute('class').trim();
-       
-         if(cl == 'sub'){
-            uls[3].classList.remove('sub');
-            uls[3].classList.add('act');
-         }else if(cl == 'act'){
-            uls[3].classList.remove('act');
-            uls[3].classList.add('sub');
-         }
-     })
+   
 
 
   
--- proyecto/js/login.js
+++ proyecto/js/login.js
@@ -3,10 +3,6 @@ var bandera_sesion = false;
 function iniciar_controles_login() {
     var btn_login = document.getElementById("iniciar_sesion");
 
-    datos[2] = "ola";
-    datos[3] = "123";
-
-
     btn_login.addEventListener("click", function () {
         var login_correo = document.getElementById("correo").value;
         var login_contra = document.getElementById("contra").value;
@@ -22,6 +18,8 @@ function psesion() {
         
         contenedor.innerHTML = "";
         contenedor.innerHTML = htmlVista;
+        barraLateralDesplegar();
+        agregarVista();
         document.getElementById('mostrar_dinero').innerHTML = monto;
     }
 }
@@ -42,3 +40,120 @@ function iniciar_login(correo, contra) {
         alert("No logeado");
     }
 }
+function barraLateralDesplegar(){
+
+    var uls = document.querySelectorAll('#VistaUl .Primer .sub');
+    var lis = document.querySelectorAll('#VistaUl .Primer');
+ 
+    lis[0].addEventListener('click',function(){
+        
+         let cl = uls[0].getAttribute('class').trim();
+       
+         if(cl == 'sub'){
+            uls[0].classList.remove('sub');
+            uls[0].classList.add('act');
+         }else if(cl == 'act'){
+            uls[0].classList.remove('act');
+            uls[0].classList.add('sub');
+         }
+     })
+     lis[1].addEventListener('click',function(){
+        
+         let cl = uls[1].getAttribute('class').trim();
+       
+         if(cl == 'sub'){
+            uls[1].classList.remove('sub');
+            uls[1].classList.add('act');
+         }else if(cl == 'act'){
+            uls[1].classList.remove('act');
+            uls[1].classList.add('sub');
+         }
+     })
+     lis[2].addEventListener('click',function(){
+        
+         let cl = uls[2].getAttribute('class').trim();
+       
+         if(cl == 'sub'){
+            uls[2].classList.remove('sub');
+            uls[2].classList.add('act');
+         }else if(cl == 'act'){
+            uls[2].classList.remove('act');
+            uls[2].classList.add('sub');
+         }
+     })
+     lis[3].addEventListener('click',function(){
+        
+         let cl = uls[3].getAttribute('class').trim();
+       
+         if(cl == 'sub'){
+            uls[3].classList.remove('sub');
+            uls[3].classList.add('act');
+         }else if(cl == 'act'){
+            uls[3].classList.remove('act');
+            uls[3].classList.add('sub');
+         }
+     })
+
+}
+
+function agregarVista(){
+    subVista = document.querySelectorAll('.vistas');
+    
+    subVista[0].addEventListener('click',function(){
+        document.getElementsByClassName('info')[0].innerHTML = "";
+        document.getElementsByClassName('info')[0].innerHTML = VistasTotales[0];
+    })
+
+    subVista[1].addEventListener('click',function(){
+        document.getElementsByClassName('info')[0].innerHTML = "";
+        document.getElementsByClassName('info')[0].innerHTML = VistasTotales[1];
+    })
+
+    subVista[2].addEventListener('click',function(){
+        document.getElementsByClassName('info')[0].innerHTML = "";
+        document.getElementsByClassName('info')[0].innerHTML = VistasTotales[2];
+    })
+
+    subVista[3].addEventListener('click',function(){
+        document.getElementsByClassName('info')[0].innerHTML = "";
+        document.getElementsByClassName('info')[0].innerHTML = VistasTotales[3];
+    })
+
+    subVista[4].addEventListener('click',function(){
+        document.getElementsByClassName('info')[0].innerHTML = "";
+        document.getElementsByClassName('info')[0].innerHTML = VistasTotales[4];
+    })
+
+    subVista[5].addEventListener('click',function(){
+        document.getElementsByClassName('info')[0].innerHTML = "";
+        document.getElementsByClassName('info')[0].innerHTML = VistasTotales[5];
+    })
+
+    subVista[6].addEventListener('click',function(){
+        document.getElementsByClassName('info')[0].innerHTML = "";
+        document.getElementsByClassName('info')[0].innerHTML = VistasTotales[6];
+    })
+
+
+    subVista[7].addEventListener('click',function(){
+        document.getElementsByClassName('info')[0].innerHTML = "";
+        document.getElementsByClassName('info')[0].innerHTML = VistasTotales[7];
+    })
+
+    subVista[8].addEventListener('click',function(){
+        document.getElementsByClassName('info')[0].innerHTML = "";
+        document.getElementsByClassName('info')[0].innerHTML = VistasTotales[8];
+    })
+
+    subVista[9].addEventListener('click',function(){
+        document.getElementsByClassName('info')[0].innerHTML = "";
+        document.getElementsByClassName('info')[0].innerHTML = VistasTotales[9];
+    })
+
+
+    document.getElementById('cerrar').addEventListener('click',function(){
+        contenedor.innerHTML = "";
+        contenedor.innerHTML = htmlIndex;
+    })
+}
+
